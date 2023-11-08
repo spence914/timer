e2d5c5537e32474278ac49ab187208e7a7abc627
Spencer Lewis <spence.914@gmail.com>
2023-11-08 13:47:22 -0800

    add comments to clarify structure of function

--- timer1.js
+++ timer1.js
@@ -1,7 +1,10 @@
-const sortedTimes = process.argv.slice(2).sort((a, b) => a - b);
+// Timer that takes command line arguments and has system beep at each defined interval.
+
+
+const sortedTimes = process.argv.slice(2).sort((a, b) => a - b); // Takes times passed to command line and sorts them by ascending order.
 
 for (let time of sortedTimes) {
-  if (time < 0 || isNaN(time)) {
+  if (time < 0 || isNaN(time)) { // If time passed is negative or not a number it is skipped entirely
     continue;
   }
   setTimeout(() => {
