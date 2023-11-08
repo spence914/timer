f8c94fcbef01e126595262de9110701e4670a554
Spencer Lewis <spence.914@gmail.com>
2023-11-08 13:41:05 -0800

    add checks for if time given is negative or not a number, loop is told to skip those values and continue

--- timer1.js
+++ timer1.js
@@ -2,6 +2,9 @@ const times = process.argv.slice(2);
 const sortedTimes = times.sort((a, b) => a - b);
 
 for (let time of sortedTimes) {
+  if (time < 0 || isNaN(time)) {
+    continue;
+  }
   setTimeout(() => {
     process.stdout.write('\x07');
   },(1000 * time));
