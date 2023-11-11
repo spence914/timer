5d427ff2e0c391c944d714bd832f0b2cfa8731b3
Spencer Lewis <spence.914@gmail.com>
2023-11-11 12:39:52 -0800

    implement timer functionality on numeric keypress, add sourcing for documentation

--- timer2.js
+++ timer2.js
@@ -1,8 +1,17 @@
+// Readline and keypress documentation from https://nodejs.org/api/readline.html and https://stackoverflow.com/questions/55181297/where-can-i-find-documentation-of-the-keypress-event-in-node-js
+
 const readline = require('readline');
 
 const goBeep = () => {
   process.stdout.write('\x07');
 };
+
+const setTimer = (seconds) => {
+  console.log(`Setting timer for ${seconds} seconds...`);
+  setTimeout(goBeep, 1000 * seconds);
+};
+
+const numKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 readline.emitKeypressEvents(process.stdin);
 
@@ -18,6 +27,10 @@ process.stdin.on('keypress', (str, key) => {
   if (key.ctrl && key.name === 'c') {
     console.log("Thanks for using me, ciao!");
     process.exit();
+  }
+
+  if (numKeys.includes(key.name)) {
+    setTimer(key.name);
   }
 });
 
